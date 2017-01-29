cdf41c74de303dd85975b4dfb4597bcd83dca24f
magicffourier <iscream2333@gmail.com>
2017-01-29 20:18:01 +0800

    first attempt

---
+++ os/linux/.rt2870sta.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/opt/2010_0709_RT2870_Linux_STA_v2.4.0.1/os/linux/rt2870sta.ko := arm-linux-ld -r  -T /opt/FriendlyARM/linux-3.4.y-nanopi2-lollipop-mr1/scripts/module-common.lds --build-id  -o /opt/2010_0709_RT2870_Linux_STA_v2.4.0.1/os/linux/rt2870sta.ko /opt/2010_0709_RT2870_Linux_STA_v2.4.0.1/os/linux/rt2870sta.o /opt/2010_0709_RT2870_Linux_STA_v2.4.0.1/os/linux/rt2870sta.mod.o
